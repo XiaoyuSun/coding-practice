290ced403a82f8a30405c3ebc60b68d8fc25174a
Xiaoyu Sun <sunxy311@gmail.com>
2024-02-20 15:29:46 -0800

    Create Maximum_Product_Subarray.js

---
+++ Maximum_Product_Subarray.js
@@ -0,0 +1,25 @@
+let test1 = [-1,-1,-1,2,-3];
+
+const solution = (nums) => {
+  let res = Math.max(...nums);
+  
+  let currMax = 1;
+  let currMin = 1;
+
+  for (let num of nums) {
+    if (num === 0) {
+      currMax = 1;
+      currMin = 1;
+    }
+
+    let tmpMax = currMax;
+    currMax = Math.max(num, tmpMax * num, currMin * num)
+    currMin = Math.min(num, tmpMax * num, currMin * num)
+
+    res = Math.max(currMax, res);
+  }
+
+  return res;
+}
+
+console.log(solution(test1))
