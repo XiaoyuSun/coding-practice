5525d25c1f14d2278039ca458316453057d3ae4b
Xiaoyu Sun <sunxy311@gmail.com>
2024-02-20 12:11:22 -0800

    Create 540. Single Element in a Sorted Array.js

---
+++ 540. Single Element in a Sorted Array.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNonDuplicate = function (nums) {
+  if (nums.length === 1) return nums[0];
+
+  for (let i = 0; i < nums.length; i++) {
+    if (i === 0 && nums[i] !== nums[i + 1]) return nums[i];
+    if (i === (nums.length - 1) && nums[i] !== nums[i - 1]) return nums[i];
+
+    if (nums[i] !== nums[i + 1] && nums[i] !== nums[i - 1]) {
+      return nums[i]
+    }
+  }
+
+};
