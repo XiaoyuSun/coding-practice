5ed2cfb3c45a8eca91725018aabf0e4139b6c54d
Xiaoyu Sun <sunxy311@gmail.com>
2024-02-21 13:31:41 -0800

    Update 605 Can-Place-Flowers.js

--- 605 Can-Place-Flowers.js
+++ 605 Can-Place-Flowers.js
@@ -1,39 +1,26 @@
 /**
-* @param {number[]} flowerbed
-* @param {number} n
-* @return {boolean}
-*/
-var canPlaceFlowers = function(flowerbed, n) {
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function (flowerbed, n) {
+
   let count = n;
   let index = 0;
-  
-  console.log(flowerbed)
-  
-  while (index < flowerbed.length) {
-    console.log('index', index, 'count',count)
-    if (count === 0) return true;
-    if (index === 0 && flowerbed[index+1] === 0 && flowerbed[index] === 0 ) {
-      index
-      flowerbed[index] = 1;
-      count -= 1;
-    }
-    
-    if (flowerbed[index-1] === 0 && flowerbed[index+1] === 0 && flowerbed[index] === 0) {
-      index
-      flowerbed[index] = 1;
-      count -= 1;
-    }
 
-    if (index === flowerbed.length - 1 && flowerbed[index] === 0 && flowerbed[index-1] === 0) {
-      index
+  while (index < flowerbed.length) {
+    if (count === 0) return true;
+
+    if (flowerbed[index] === 0
+      && (flowerbed[index + 1] === 0 || flowerbed[index + 1] === undefined)
+      && (flowerbed[index - 1] === 0 || flowerbed[index - 1] === undefined)
+    ) {
       flowerbed[index] = 1;
-      count -= 1
+      count -= 1;
     }
 
     index += 1;
   }
-  count
+
   return count === 0;
 };
-
-console.log(canPlaceFlowers([0,0,1,0,1,0,1,0,1,0,0,0,0,0,0,0,0,0,1,0,0], 6));
