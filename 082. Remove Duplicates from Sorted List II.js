3dbf453f7b320d2f75f751c202e4eaae45002769
Xiaoyu Sun <sunxy311@gmail.com>
2024-02-21 11:03:27 -0800

    Update 082. Remove Duplicates from Sorted List II.js

--- 082. Remove Duplicates from Sorted List II.js
+++ 082. Remove Duplicates from Sorted List II.js
@@ -9,6 +9,23 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var deleteDuplicates = function(head) {
-    
+var deleteDuplicates = function (head) {
+  let sentinel = new ListNode(-1, head);
+  let predecessor = sentinel;
+
+  while (head) {
+    if (head.next && head.val === head.next.val) {
+      while (head.next && head.val === head.next.val) {
+        head = head.next;
+      }
+
+      predecessor.next = head.next;
+    } else {
+      predecessor = predecessor.next;
+    }
+
+    head = head.next;
+  }
+
+  return sentinel.next
 };
