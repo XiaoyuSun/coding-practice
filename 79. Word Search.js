0292189aa9be9b7f64dbb7c85f346dac80f8b8b0
Xiaoyu Sun <sunxy311@gmail.com>
2024-04-03 08:58:22 -0700

    Word Search

---
+++ 79. Word Search.js
@@ -0,0 +1,32 @@
+/**
+ * @param {character[][]} board
+ * @param {string} word
+ * @return {boolean}
+ */
+var exist = function (board, word) {
+  let rowLen = board.length;
+  let colLen = board[0].length;
+
+  function dfs(r, c, index) {
+      if (index === word.length) return true;
+
+      if (r < 0 || r >= rowLen || c < 0 || c >= colLen || board[r][c] !== word[index]) return false;
+
+      const tmp = board[r][c];
+      board[r][c] = '$';
+
+      const found = dfs(r + 1, c, index + 1) || dfs(r - 1, c, index + 1) || dfs(r, c + 1, index + 1) || dfs(r, c - 1, index + 1);
+
+      board[r][c] = tmp;
+
+      return found;
+  }
+
+  for (let row = 0; row < rowLen; ++row) {
+      for (let col = 0; col < colLen; ++col) {
+          if (dfs(row, col, 0)) return true;
+      }
+  }
+
+  return false;
+};
