1c20a62565dba5c733862eeacb139a3401ed5a2a
Xiaoyu Sun <sunxy311@gmail.com>
2024-04-04 08:27:27 -0700

    Create 1614. Maximum Nesting Depth of the Parentheses.js

---
+++ 1614. Maximum Nesting Depth of the Parentheses.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maxDepth = function (s) {
+    let ans = 0;
+    let stack = [];
+
+    for (let char of s) {
+        if (char === '(') {
+            stack.push(char)
+        } else if (char === ')') {
+            ans = Math.max(ans, stack.length);
+            stack.pop();
+        }
+    }
+
+    return ans;
+};
