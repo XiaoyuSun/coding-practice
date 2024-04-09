f1203ffd63a66c9835ce04deb5cf0fcf877359ef
Xiaoyu Sun <sunxy311@gmail.com>
2024-04-09 11:44:19 -0700

    Create 2073. Time Needed to Buy Tickets.js

---
+++ 2073. Time Needed to Buy Tickets.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} tickets
+ * @param {number} k
+ * @return {number}
+ */
+var timeRequiredToBuy = function (tickets, k) {
+    let ans = 0;
+
+    while (tickets[k] > 0) {
+        for (let index in tickets) {
+            if (tickets[k] === 0) break;
+            if (tickets[index] > 0) {
+                ++ans;
+                tickets[index] -= 1;
+            }
+        }
+    }
+
+    return ans;
+};
