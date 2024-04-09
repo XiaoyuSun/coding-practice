e9ce725a6fc7cde0907ea29edbc7458ae6c12992
Xiaoyu Sun <sunxy311@gmail.com>
2024-04-09 11:45:16 -0700

    Create 1700. Number of Students Unable to Eat Lunch.js

---
+++ 1700. Number of Students Unable to Eat Lunch.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} students
+ * @param {number[]} sandwiches
+ * @return {number}
+ */
+var countStudents = function (students, sandwiches) {
+    const count = [0, 0];
+
+    for (const student of students) {
+        count[student]++;
+    }
+
+    for (let i = 0; i < sandwiches.length; i++) {
+        if (count[sandwiches[i]] === 0) {
+
+            return sandwiches.length - i;
+        }
+        count[sandwiches[i]]--;
+    }
+
+    return 0;
+};
